5bcd6e30228d76695c17831b6c542d905d5f6794
Jeremy Lin <cheyulin0222@gmail.com>
2025-05-02 16:29:11 +0800

    add git config

---
+++ src/main/java/com/arplanets/auth/config/GitConfig.java
@@ -0,0 +1,9 @@
+package com.arplanets.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:git.properties")
+public class GitConfig {
+}
